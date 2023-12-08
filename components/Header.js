0a155e6128d8e275925d36e091a0792f6e37765d
muhammadtouqeer1991 <toqeerabbasi7@gmail.com>
2023-12-08 13:37:58 +0500

    updateCode

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ import Headerimg from './Headerimg'
 const Header = () => {
 return (
 <>
-<div className="headerSec bg-topbarcolor relative z-0 overflow-hidden z-0">
+<div className="headerSec bg-topbarcolor relative z-0 overflow-hidden">
     <div className='flex container justify-between mx-auto py-7 pt-[3rem]'>
         <Headerinfo />
         <Headerform />
